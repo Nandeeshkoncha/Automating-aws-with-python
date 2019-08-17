6f691aa3dd5e305ce60a20cefbd663319813d0e5
Nandeeswara reddy Koncha <nandeeswarareddykoncha@NandeeswarasMBP.hsd1.tx.comcast.net>
2019-08-17 18:05:51 -0500

    refactored nantron python file

--- 01-nantron/nantron/bucket.py
+++ 01-nantron/nantron/bucket.py
@@ -1,19 +1,75 @@
 # -*- coding: utf-8 -*-
+import mimetypes
+from botocore.exceptions import ClientError
+from pathlib import Path
 
-""" Classes for s3 Buckets."""
+"""Classes for s3 Buckets."""
 
 class BucketManager:
-    """ Manage s3 bucket"""
+    """Manage s3 bucket."""
 
     def __init__(self,session):
-        """ create a BucketManager object."""
+        """Create a BucketManager object."""
+        self.session = session
         self.s3 = session.resource('s3')
 
     def list_buckets(self):
-        """ list buckets """
+        """List buckets"""
         return self.s3.buckets.all()
 
     def list_bucket_objects(self,bucket):
-        """ list bucket objects """
+        """List bucket objects"""
         return self.s3.Bucket(bucket).objects.all()
+
+    def init_bucket(self,bucket_name):
+        s3_bucket = None
+        try:
+            s3_bucket = self.s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': self.session.region_name})
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+                s3_bucket = self.s3.Bucket(bucket_name)
+            else:
+                raise e
+        return s3_bucket
+
+    def set_policy(self, bucket):
+        policy = """
+         {
+         "Version":"2012-10-17",
+          "Statement":[{
+          "Sid":"PublicReadGetObject",
+          "Effect":"Allow",
+          "Principal": "*",
+          "Action":["s3:GetObject"],
+          "Resource":["arn:aws:s3:::%s/*"]
+          }]
+         }""" % bucket.name
+        policy = policy.strip()
+        pol = bucket.Policy()
+        pol.put(Policy=policy)
+
+    def configure_website(self, bucket):
+        bucket.Website().put(WebsiteConfiguration={'ErrorDocument': {'Key': 'error.html'}, 'IndexDocument': {'Suffix': 'index.html'}})
         
+    @staticmethod
+    def upload_file(bucket, path, key):
+        """ static Method because of no need of self."""
+
+        """It is to upload file."""
+        content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+        return bucket.upload_file(path, key, ExtraArgs={'ContentType': content_type})
+
+    def sync(self, pathname, bucket_name):
+        """Sync directory."""
+        bucket = self.s3.Bucket(bucket_name)
+
+        root = Path(pathname).expanduser().resolve()
+        """this is to remove tilda's"""
+
+        def handle_dir(target):
+            for p in target.iterdir():
+                if p.is_dir():
+                    handle_dir(p)
+                if p.is_file():
+                    self.upload_file(bucket, str(p), str(p.relative_to(root)))
+        handle_dir(root)
--- 01-nantron/nantron/nantron.py
+++ 01-nantron/nantron/nantron.py
@@ -4,11 +4,9 @@
 
 """Nantron: Deploy websites with aws."""
 
-from pathlib import Path
-import mimetypes
+
 import boto3
 import click
-from botocore.exceptions import ClientError
 from bucket import BucketManager
 
 
@@ -41,56 +39,21 @@ def list_bucket_objects(bucket):
 @click.argument('bucketname')
 def setup_bucket(bucketname):
     """Create new s3 bucket."""
-    s3_bucket = None
-    try:
-        s3_bucket = s3.create_bucket(Bucket=bucketname, CreateBucketConfiguration={'LocationConstraint': session.region_name})
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
-            s3_bucket = s3.Bucket(bucketname)
-            print(s3_bucket)
-        else:
-            raise e
-    policy = """
-     {
-     "Version":"2012-10-17",
-      "Statement":[{
-      "Sid":"PublicReadGetObject",
-      "Effect":"Allow",
-      "Principal": "*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::%s/*"]
-      }]
-     }""" % s3_bucket.name
-
-    policy = policy.strip()
-    pol = s3_bucket.Policy()
-    pol.put(Policy=policy)
-    bucket_website = s3_bucket.Website()
-    bucket_website.put(WebsiteConfiguration={'ErrorDocument': {'Key': 'error.html'}, 'IndexDocument': {'Suffix': 'index.html'}})
+    s3_bucket = bucket_manager.init_bucket(bucketname)
+    bucket_manager.set_policy(s3_bucket)
+    bucket_manager.configure_website(s3_bucket)
     return
 
 
-def upload_file(s3_bucket, path, key):
-    """It is to upload file."""
-    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
-    s3_bucket.upload_file(path, key, ExtraArgs={'ContentType': content_type})
+
 
 
 @cli.command('sync')
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
 def sync(pathname, bucket):
-    """Sync directory."""
-    s3_bucket = s3.Bucket(bucket)
-    root = Path(pathname).expanduser().resolve()
-    "this is to remove tilda's"
-    def handle_dir(target):
-        for p in target.iterdir():
-            if p.is_dir():
-                handle_dir(p)
-            if p.is_file():
-                upload_file(s3_bucket, str(p), str(p.relative_to(root)))
-    handle_dir(root)
+    """ Sync contents to s3 bucket."""
+    bucket_manager.sync(pathname, bucket)
 
 
 if __name__ == '__main__':
